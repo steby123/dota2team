0c14965d8a7f037e19fee4a3e80d062d4d85deac
steby123 <steby.yeo@gmail.com>
2022-10-18 17:54:35 +0700

    first

---
+++ background.js
@@ -0,0 +1,15 @@
+const parentCoantainer = document.querySelector('.read-more-container ');
+
+parentCoantainer.addEventListener('click',event=>{
+  const current = event.target;
+  const isReadMoreBtn = current.className.includes('read-more-btn');
+  if(!isReadMoreBtn) 
+  return;
+  
+  const currentText = event.target.parentNode.querySelector('.read-more-text');
+  
+  currentText.classList.toggle('read-more-text--show');
+  
+  current.textContent=current.textContent.includes('Read More...') ?
+  "Read Less..." : "Read More...";
+})
---
+++ style.js
@@ -0,0 +1,9 @@
+const menuToogle = document.querySelector('.menuToggle');
+const navigation = document.querySelector('.navigation');
+menuToogle.onclick = function(){
+    navigation.classList.toggle('open');
+ }
+
+
+ 
+ 
